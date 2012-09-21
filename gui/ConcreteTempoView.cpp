85de244df0cb82b609e8f720e50c2ae5fac64b9b
kbinani <kbinani@msn.com>
2012-09-21 19:22:21 +0900

    Merge branch 'devel'

--- gui/ConcreteTempoView.cpp
+++ gui/ConcreteTempoView.cpp
@@ -5,7 +5,8 @@
  * This file is part of cadencii.
  *
  * cadencii is free software; you can redistribute it and/or
- * modify it under the terms of the BSD License.
+ * modify it under the terms of the GNU General Public License
+ * version 2.0 as published by the Free Software Foundation.
  *
  * cadencii is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
