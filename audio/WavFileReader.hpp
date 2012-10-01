4af49e044bbd7ab947415f8c653c317e3386f043
kbinani <kbinani@msn.com>
2012-10-02 04:53:03 +0900

    Refactor audio/WavFileReader

--- audio/WavFileReader.hpp
+++ audio/WavFileReader.hpp
@@ -25,10 +25,22 @@
 namespace cadencii{
 namespace audio{
 
+    /**
+     * @brief WAV ファイルから、波形を読み込むクラス
+     */
     class WavFileReader : public Sender{
     private:
+        /**
+         * @brief 内部で使用するバッファの長さ
+         */
         static const int unitBufferLength = 6400;
+        /**
+         * @brief 内部バッファ(左チャンネル)
+         */
         double bufferLeft[unitBufferLength];
+        /**
+         * @brief 内部バッファ(右チャンネル)
+         */
         double bufferRight[unitBufferLength];
         /**
          * @brief bufferLeft, bufferRight に入っているバッファの長さ
@@ -39,16 +51,33 @@ namespace audio{
          * @brief bufferLeft, bufferRight を次回呼ぶ際は、bufferIndex 番目からを使う
          */
         int bufferIndex;
-
-        std::basic_ifstream<char> m_stream;//TODO: rename
-        int m_channel;//TODO: rename
-        int m_sample_per_sec;//TODO: rename
-        int m_byte_per_sample;//TODO: rename
-        uint64_t m_total_samples;//TODO: rename
-        uint64_t m_header_offset;//TODO: rename
-        bool m_opened;
+        /**
+         * @brief ファイルストリーム
+         */
+        std::basic_ifstream<char> stream;
+        /**
+         * @brief チャンネル数
+         */
+        int channels;
+        /**
+         * @brief サンプリングレート(s^-1)
+         */
+        int sampleRate;
+        /**
+         * @brief 1 チャンネル、1 サンプルあたりのバイト数
+         */
+        int bytesPerSample;
+        /**
+         * @brief ファイルストリームが正しく open できているかどうか
+         */
+        bool isReady;
 
     public:
+        /**
+         * @brief WAV ファイルのファイルパスを指定し、初期化する
+         * @param filePath ファイルパス
+         * @todo Sender( 44100 ) となっている箇所、これでよいか検討する
+         */
         explicit WavFileReader( const std::string &filePath ) :
             Sender( 44100 )
         {
@@ -57,13 +86,15 @@ namespace audio{
             bufferFilledLength = 0;
             bufferIndex = 0;
 
-            m_channel = 2;
-            m_sample_per_sec = 44100;
-            m_byte_per_sample = 2;
-            m_total_samples = 0;
-            m_header_offset = 0;
-            m_opened = false;
+            channels = 2;
+            sampleRate = 44100;
+            bytesPerSample = 2;
+            isReady = false;
             open( filePath );
+        }
+
+        ~WavFileReader(){
+            close();
         }
 
         void pull( double *left, double *right, int length ){
@@ -101,148 +132,157 @@ namespace audio{
             }
         }
 
+        /**
+         * @brief ファイルを閉じる
+         */
         void close(){
-            if( m_opened ){
-                m_stream.close();
-                m_opened = false;
+            if( isReady ){
+                stream.close();
+                isReady = false;
             }
         }
 
     private:
+        /**
+         * @brief ファイルを開き、WAV のフォーマット情報を読み込む
+         * @param filePath 開くファイルのパス
+         * @return フォーマット情報の読み込みに成功した場合に true を返す
+         */
         bool open( const std::string &filePath ){
-            m_stream.open( filePath.c_str(), std::ios::binary );
+            stream.open( filePath.c_str(), std::ios::binary );
 
             // RIFF
             char buf[4];
-            m_stream.read( buf, sizeof( char ) * 4 );
-            if( m_stream.gcount() != 4 || buf[0] != 'R' || buf[1] != 'I' || buf[2] != 'F' || buf[3] != 'F' ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 4 );
+            if( stream.gcount() != 4 || buf[0] != 'R' || buf[1] != 'I' || buf[2] != 'F' || buf[3] != 'F' ){
+                stream.close();
                 return false;
             }
 
             // ファイルサイズ - 8最後に記入
-            m_stream.read( buf, sizeof( char ) * 4 );
-            if( m_stream.gcount() != 4 ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 4 );
+            if( stream.gcount() != 4 ){
+                stream.close();
                 return false;
             }
 
             // WAVE
-            m_stream.read( buf, sizeof( char ) * 4 );
-            if( m_stream.gcount() != 4 || buf[0] != 'W' || buf[1] != 'A' || buf[2] != 'V' || buf[3] != 'E' ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 4 );
+            if( stream.gcount() != 4 || buf[0] != 'W' || buf[1] != 'A' || buf[2] != 'V' || buf[3] != 'E' ){
+                stream.close();
                 return false;
             }
 
             // fmt
-            m_stream.read( buf, sizeof( char ) * 4 );
-            if( m_stream.gcount() != 4 || buf[0] != 'f' || buf[1] != 'm' || buf[2] != 't' || buf[3] != ' ' ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 4 );
+            if( stream.gcount() != 4 || buf[0] != 'f' || buf[1] != 'm' || buf[2] != 't' || buf[3] != ' ' ){
+                stream.close();
                 return false;
             }
 
             // fmt チャンクのサイズ
-            m_stream.read( buf, sizeof( char ) * 4 );
-            if( m_stream.gcount() != 4 ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 4 );
+            if( stream.gcount() != 4 ){
+                stream.close();
                 return false;
             }
             uint64_t chunksize = (uint64_t)VSQ_NS::BitConverter::makeUInt32LE( buf );
-            uint64_t fmt_chunk_end_location = (uint64_t)m_stream.tellg() + chunksize;
+            uint64_t fmt_chunk_end_location = (uint64_t)stream.tellg() + chunksize;
 
             // format ID
-            m_stream.read( buf, sizeof( char ) * 2 );
-            if( m_stream.gcount() != 2 ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 2 );
+            if( stream.gcount() != 2 ){
+                stream.close();
                 return false;
             }
 
             // チャンネル数
-            m_stream.read( buf, sizeof( char ) * 2 );
-            if( m_stream.gcount() != 2 ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 2 );
+            if( stream.gcount() != 2 ){
+                stream.close();
                 return false;
             }
-            m_channel = buf[1] << 8 | buf[0];
+            channels = buf[1] << 8 | buf[0];
 
             // サンプリングレート
-            m_stream.read( buf, sizeof( char ) * 4 );
-            if( m_stream.gcount() != 4 ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 4 );
+            if( stream.gcount() != 4 ){
+                stream.close();
                 return false;
             }
-            m_sample_per_sec = (int)VSQ_NS::BitConverter::makeUInt32LE( buf );
+            sampleRate = (int)VSQ_NS::BitConverter::makeUInt32LE( buf );
 
             // データ速度
-            m_stream.read( buf, sizeof( char ) * 4 );
-            if( m_stream.gcount() != 4 ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 4 );
+            if( stream.gcount() != 4 ){
+                stream.close();
                 return false;
             }
 
             // ブロックサイズ
-            m_stream.read( buf, sizeof( char ) * 2 );
-            if( m_stream.gcount() != 2 ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 2 );
+            if( stream.gcount() != 2 ){
+                stream.close();
                 return false;
             }
 
             // サンプルあたりのビット数
-            m_stream.read( buf, sizeof( char ) * 2 );
-            if( m_stream.gcount() != 2 ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 2 );
+            if( stream.gcount() != 2 ){
+                stream.close();
                 return false;
             }
             int bit_per_sample = buf[1] << 8 | buf[0];
-            m_byte_per_sample = bit_per_sample / 8;
+            bytesPerSample = bit_per_sample / 8;
 
             // 拡張部分
-            m_stream.seekg( fmt_chunk_end_location, std::ios::beg );
+            stream.seekg( fmt_chunk_end_location, std::ios::beg );
 
             // data
-            m_stream.read( buf, sizeof( char ) * 4 );
-            if( m_stream.gcount() != 4 || buf[0] != 'd' || buf[1] != 'a' || buf[2] != 't' || buf[3] != 'a' ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 4 );
+            if( stream.gcount() != 4 || buf[0] != 'd' || buf[1] != 'a' || buf[2] != 't' || buf[3] != 'a' ){
+                stream.close();
                 return false;
             }
 
             // size of data chunk
-            m_stream.read( buf, sizeof( char ) * 4 );
-            if( m_stream.gcount() != 4 ){
-                m_stream.close();
+            stream.read( buf, sizeof( char ) * 4 );
+            if( stream.gcount() != 4 ){
+                stream.close();
                 return false;
             }
             int size = (int)VSQ_NS::BitConverter::makeUInt32LE( buf );
-            m_total_samples = size / (m_channel * m_byte_per_sample);
 
-            m_opened = true;
-            m_header_offset = (uint64_t)m_stream.tellg();
+            isReady = true;
             return true;
         }
 
         /**
-         * @brief ストリームから bufferLeft, bufferRight に読み込む
+         * @brief ストリームから内部バッファに読み込む
          */
         void fillin(){
-            if( m_byte_per_sample == 2 ){
-                if( m_channel == 2 ){
+            if( bytesPerSample == 2 ){
+                if( channels == 2 ){
                     fillinStereo16();
                 }
             }
         }
 
         /**
-         * @brief ステレオ、16 bit per sample と見なして、ストリームから読み込む
+         * @brief ステレオ、16 bit per sample と見なして、ストリームから波形を読み込み、内部バッファに格納する
          */
         void fillinStereo16(){
-            const int bufferLength = unitBufferLength * (16 / 8) * 2;
-            char buffer[bufferLength];
-            m_stream.read( buffer, bufferLength );
-            int actualBufferLegnth = m_stream.gcount() / (16 / 8) / 2;
+            const int bufferLength = unitBufferLength * bytesPerSample * 2;
+            char temporaryBuffer[bufferLength];
+            stream.read( temporaryBuffer, bufferLength );
+            int actualBufferLegnth = stream.gcount() / bytesPerSample / 2;
             const double coeff = 1.0 / 32768.0;
+            char *temporaryBufferIndex = temporaryBuffer;
             for( int i = 0; i < actualBufferLegnth; i++ ){
-                bufferLeft[i] = VSQ_NS::BitConverter::makeInt16LE( buffer + sizeof( char ) * (i * 4) ) * coeff;
-                bufferRight[i] = VSQ_NS::BitConverter::makeInt16LE( buffer + sizeof( char ) * (i * 4 + 2) ) * coeff;
+                bufferLeft[i] = VSQ_NS::BitConverter::makeInt16LE( temporaryBufferIndex ) * coeff;
+                temporaryBufferIndex += bytesPerSample;
+                bufferRight[i] = VSQ_NS::BitConverter::makeInt16LE( temporaryBufferIndex ) * coeff;
+                temporaryBufferIndex += bytesPerSample;
             }
             bufferFilledLength = actualBufferLegnth;
             bufferIndex = 0;
