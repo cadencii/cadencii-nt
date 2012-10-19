f63f168f2bfd052db2c957f03d12cc3565bf8169
kbinani <kbinani@msn.com>
2012-10-20 01:20:20 +0900

    Update release-tools/create_default_pot.sh

--- release-tools/create_default_pot.sh
+++ release-tools/create_default_pot.sh
@@ -3,9 +3,16 @@
 workingDirectory=$(cd $(dirname $0);pwd)
 
 (
+    qmake ${workingDirectory}/../Cadencii.pro -o ${workingDirectory}/Makefile
+    rm -f ${workingDirectory}/../bin/release/ui_*.h
+    rm -f ${workingDirectory}/../bin/debug/ui_*.h
+    cd ${workingDirectory}
+    make compiler_uic_clean
+    make compiler_uic_make_all
+
     rm -f ${workingDirectory}/default.pot.files
     cd ${workingDirectory}/../
-    find . -type f -name \*.cpp -or -name \*.h | grep -v 'qt-solutions' | sed 's:^[.]/\(.*\)$:\1:g' > ${workingDirectory}/default.pot.files
+    find . -type f -name \*.cpp -or -name \*.h | grep -v 'qt-solutions' | grep -v 'ui-test' | sed 's:^[.]/\(.*\)$:\1:g' > ${workingDirectory}/default.pot.files
     lupdate @${workingDirectory}/default.pot.files -ts ${workingDirectory}/../default.pot.ts
     lconvert ${workingDirectory}/../default.pot.ts -o ${workingDirectory}/../default.pot
     rm -f ${workingDirectory}/default.pot.files
