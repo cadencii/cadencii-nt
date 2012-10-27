b5c397440556fdb5cb64dea77d3042942a94db1e
kbinani <kbinani@msn.com>
2012-10-28 01:01:26 +0900

    Modify qt/gui/PropertyTreeUpdateWorker, add license terms

--- qt/gui/PropertyTreeUpdateWorker.cpp
+++ qt/gui/PropertyTreeUpdateWorker.cpp
@@ -1,3 +1,17 @@
+/**
+ * PropertyTreeUpdateWorker.cpp
+ * Copyright © 2012 kbinani
+ *
+ * This file is part of cadencii.
+ *
+ * cadencii is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2.0 as published by the Free Software Foundation.
+ *
+ * cadencii is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
 #include "PropertyTreeUpdateWorker.hpp"
 
 namespace cadencii{
