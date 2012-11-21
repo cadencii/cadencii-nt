58d307feca0a507baffd00947e29ba02a282a6bf
kbinani <kbinani@msn.com>
2012-11-22 01:22:41 +0900

    Modify ci-tool/run_ui_test.sh

--- ci-tool/run_ui_test.sh
+++ ci-tool/run_ui_test.sh
@@ -8,13 +8,11 @@ function run_ui_test { (
     local qmake=$(which_qmake)
 
     cd ${workspace}/ui-test
-    rm -f bin/*
     rm -f Makefile
     ${qmake} CONFIG+=declarative_debug -o Makefile ui-test.pro
     make
     xterm -display localhost:1 -e ${workspace}/ui-test/bin/ui-test -xunitxml -o ${workspace}/ui-test/result.xml
     cd ${workspace}/ui-test
-    make clean
     echo "[$(date +"%Y-%m-%d %H:%M:%S")] $0 end"
 ) }
 
