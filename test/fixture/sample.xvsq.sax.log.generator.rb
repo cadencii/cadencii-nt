2e81dabebf3805dab8feedfbdbafb754370e09bb
kbinani <kbinani@msn.com>
2012-12-02 18:22:18 +0900

    Modify fixture/sample.xvsq.sax.log.generator.rb, coding style(tab)

--- test/fixture/sample.xvsq.sax.log.generator.rb
+++ test/fixture/sample.xvsq.sax.log.generator.rb
@@ -4,22 +4,22 @@ require "rexml/parsers/sax2parser"
 require "rexml/sax2listener"
 
 class Listener
-		include REXML::SAX2Listener
+  include REXML::SAX2Listener
 
   def initialize output_handle
-  		@handle = output_handle
+    @handle = output_handle
   end
 
   def start_element uri, localname, qname, attributes
-  	 @handle.puts("startElement\t#{qname}")
+    @handle.puts("startElement\t#{qname}")
   end
 
   def end_element uri, localname, qname
-  		@handle.puts("endElement\t#{qname}")
+    @handle.puts("endElement\t#{qname}")
   end
 
   def characters ch
-  		@handle.puts("characters\t#{ch}")
+    @handle.puts("characters\t#{ch}")
   end
 end
 
