9e31352380ec763266f3487f78f7d9cc4a2bbd31
kbinani <kbinani@msn.com>
2012-12-05 00:36:50 +0900

    Modify ci-tool/run_test.sh to fix bug; some test code was not compiled

--- ci-tool/run_test.sh
+++ ci-tool/run_test.sh
@@ -6,8 +6,9 @@ function run_test { (
     local workspace=$(cd $(dirname $0)/../; pwd)
     rm -f ${workspace}/test/cppunit.xml
     rm -f ${workspace}/coverage.xml
-    g++ ${workspace}/test/*.cpp ${workspace}/test/**/*.cpp -w -coverage -ldl -lcppunit -o ${workspace}/test.exe
-    ${workspace}/test.exe -o ${workspace}/test/cppunit.xml -f ${workspace}/test
+    cd ${workspace}/test
+    make test.exe
+    ./test.exe -o ${workspace}/test/cppunit.xml -f ${workspace}/test
     echo "[$(date +"%Y-%m-%d %H:%M:%S")] $0 end"
 ) }
 
